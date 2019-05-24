d690c192795c0ca134e88141b868e29d70fbdf2e
layla dantas <laylatdantas@gmail.com>
2019-05-24 00:26:19 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -19,10 +19,6 @@ def bonus
       ]
    }
   }
-
-
- 
-def bonus 
   epic_tragedy[:montague][:hero][:status] = "dead"
   epic_tragedy[:capulet][:heroine][:status] = "dead"
   #code your solution here:
